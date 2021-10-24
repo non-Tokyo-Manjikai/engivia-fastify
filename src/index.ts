3dfe3baf1bc7f323e866443ea000cb7907ee7e5b
aopontann <aopontan0416@gmail.com>
2021-10-24 15:07:45 +0000

    ✨ import router

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
+import { router } from './router';
 
-const prisma = new PrismaClient();
 const app = fastify();
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '8000';
+
+app.register(router);
 
 app.get('/', async (req, res) => {
   return { hello: 'world!!!' };
 });
-
-app.post<{
-  Body: { name: string | undefined; email: string };
-}>(`/signup`, async (req, res) => {
-  const { name, email } = req.body;
-
-  const result = await prisma.user.create({
-    data: {
-      name,
-      email,
-    },
-  });
-  res.send(result);
-});
-
-app.get<{
-  Params: { id: number };
-}>(`/user/:id`, async (req, res) => {
-  const { id } = req.params;
-
-  const post = await prisma.user.findUnique({
-    where: { id: Number(id) },
-  });
-  res.send(post);
-});
-
-app.delete<{
-  Params: { id: number }
-}>(`/user/:id`, async (req, res) => {
-  const { id } = req.params;
-
-  const user_del = await prisma.user.delete({
-    where: { id: Number(id) },
-  });
-  res.send(user_del);
-})
 
 // Run the server!
 const start = async () => {
