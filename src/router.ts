aaa54fbff103cb9d904bdd6c8a39a722691e9a29
aopontann <aopontan0416@gmail.com>
2021-10-26 21:43:32 +0900

    ✨ routerに放送情報コントローラーを追加

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { slackController } from "./controller/slackController";
 import { userController } from "./controller/userController";
+import { broadcastController } from "./controller/broadcastController";
 
 export async function router(fastify: FastifyInstance): Promise<void> {
   fastify.register(slackController, { prefix: '/slack' });
   fastify.register(userController, { prefix: '/user' });
+  fastify.register(broadcastController, { prefix: '/broadcast' });
 }
