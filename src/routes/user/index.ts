dfa21297bca3c57500c74cf894da73a23b77d876
aopontann <aopontan0416@gmail.com>
2021-10-31 22:22:30 +0900

    🚚 ユーザーCRUD機能

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,44 +1,46 @@
-import { FastifyInstance } from "fastify";
-import { getUserInfo } from "../service/user/getUserInfo";
-import { updateUserInfo } from "../service/user/updateUserInfo";
-import { deleteUserInfo } from "../service/user/deleteUserInfo";
-import { uploadUserIcon } from '../service/user/uploadUserIcon';
+import { FastifyPluginAsync } from "fastify";
+import userPlugin from "./service";
 
-export async function userController(fastify: FastifyInstance): Promise<void> {
+type PutBody = { name: string, image: string, base64Image?: string }
+
+const broadcast: FastifyPluginAsync = async (fastify): Promise<void> => {
+  // Broadcastプラグインを読み込む！
+  await fastify.register(userPlugin);
   // ユーザー情報取得(idで取得)
   fastify.get<{
     Params: { id: string };
   }>(`/:id`, async (req, res) => {
     const { id } = req.params;
-    const resultUser = await getUserInfo({ id });
+    const resultUser = await fastify.getUser({ id });
     res.send(resultUser);
   });
 
   // ユーザー情報取得(bearer tokenで取得)
   fastify.get(`/`, async (req, res) => {
     const token = req?.headers?.authorization?.split(" ")[1] as string;
-    const resultUser = await getUserInfo({ token });
+    const resultUser = await fastify.getUser({ token });
     res.send(resultUser);
   });
 
   // ユーザー情報更新(bearer tokenで更新)
   fastify.put<{
-    Body: { name: string, image: string, base64Image?: string };
+    Body: PutBody;
   }>(`/`, async (req, res) => {
     const { name, image, base64Image } = req.body
     const token = req?.headers?.authorization?.split(" ")[1] as string;
-    const imageURL = base64Image ? await uploadUserIcon({ token, base64: base64Image }) : image
-    const updateUser = await updateUserInfo({ token, name, image: imageURL });
+    const imageURL = base64Image ? await fastify.uploadIcon({ token, base64: base64Image }) : image
+    const updateUser = await fastify.updateUser({ token, name, image: imageURL });
     res.send(updateUser);
   })
 
   // ユーザー情報削除(bearer tokenで削除)
   fastify.delete<{
     Params: { id: string };
-  }>(`/:id`, async (req, res) => {
-    const { id } = req.params;
+  }>(`/`, async (req, res) => {
     const token = req?.headers?.authorization?.split(" ")[1] as string;
-    const deleteUser = await deleteUserInfo({ id, token });
+    const deleteUser = await fastify.deleteUser({ token });
     res.send(deleteUser);
   })
 }
+
+export default broadcast;
