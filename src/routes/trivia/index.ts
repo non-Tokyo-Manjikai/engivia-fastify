a05e10ce3eb91bfa8c46d44ff432359eab2bd231
miyasan_0301 <miyasan.19990301@gmail.com>
2021-11-02 23:10:15 +0900

    ✨ トリビアの投稿&削除機能

--- src/routes/trivia/index.ts
+++ src/routes/trivia/index.ts
@@ -1,24 +1,51 @@
-import { FastifyPluginAsync } from "fastify";
-import triviaPlugin from "./service";
+import { FastifyPluginAsync } from 'fastify';
+import triviaPlugin from './service';
+
+type PostBody = {
+  content: string;
+  broadcastId: number;
+  token: string;
+};
 
 type PutBody = {
   id: number;
   content: string;
   token: string;
-}
+};
 
 const trivia: FastifyPluginAsync = async (fastify): Promise<void> => {
   // Triviaプラグインを読み込む！
   await fastify.register(triviaPlugin);
 
-  fastify.put<{ Body: PutBody }>(`/`, async (req, res) => {
-    const token = req?.headers?.authorization?.split(' ')[1] as string;
-    const resultUpdateBroadcastInfo = await fastify.updateTrivia({
-      ...req.body,
+  fastify.post<{ Body: PostBody }>(`/`, async (req, res) => {
+    const { content, broadcastId, token } = req.body;
+    const resultUpdateBroadcastInfo = await fastify.createTrivia({
+      content,
+      broadcastId,
       token,
     });
     res.send(resultUpdateBroadcastInfo);
   });
-}
+
+  fastify.put<{ Body: PutBody }>(`/`, async (req, res) => {
+    const { id, content, token } = req.body;
+    const resultUpdateBroadcastInfo = await fastify.updateTrivia({
+      id,
+      content,
+      token,
+    });
+    res.send(resultUpdateBroadcastInfo);
+  });
+
+  fastify.delete<{ Params: { id: number } }>(`/:id`, async (req, res) => {
+    const { id } = req.params;
+    const token = req?.headers?.authorization?.split(' ')[1] as string;
+    const resultDeleteTriviaInfo = await fastify.deleteTrivia({
+      id: Number(id),
+      token,
+    });
+    res.send(resultDeleteTriviaInfo);
+  });
+};
 
 export default trivia;
