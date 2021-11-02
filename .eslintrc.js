a82d566f7abc0693c22bea3049e86864af24429b
aopontann <aopontan0416@gmail.com>
2021-11-02 23:28:14 +0900

    🙈 .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
+    project: 'tsconfig.json',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint/eslint-plugin"],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "eslint:recommended" // 'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended', // 'plugin:prettier/recommended',
   ],
   env: {
     node: true,
     es2021: true, // jest: true,
   },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
