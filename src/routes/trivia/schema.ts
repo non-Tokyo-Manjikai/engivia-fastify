99b2f63913c76c97287cb0c8ff4e2af221942e28
aopontann <aopontan0416@gmail.com>
2021-11-02 23:33:15 +0900

    ✨ バリデーションに使うパスパラメーター、ボディリクエストのスキーマを作成

---
+++ src/routes/trivia/schema.ts
@@ -0,0 +1,17 @@
+import s from "fluent-json-schema";
+
+export const paramsGetTriviaSchema = s.object()
+  .prop('id', s.number().minimum(1).required());
+
+export const bodyPostTriviaSchema = s.object()
+  .prop('content', s.string().maxLength(100).required())
+  .prop('broadcastId', s.number().minimum(1).required())
+  .prop('token', s.string().required())
+
+export const bodyPutTriviaSchema = s.object()
+  .prop('id', s.number().minimum(1).required())
+  .prop('content', s.string().maxLength(100).required())
+  .prop('token', s.string().required())
+
+export const paramsDeleteTrivia = s.object()
+  .prop('id', s.number().minimum(1).required())
