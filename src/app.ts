d7c45f0361b6cd5b34796de61dd417258155a6f4
aopontann <aopontan0416@gmail.com>
2021-11-03 22:07:43 +0900

    ✨ add cros #14

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import { FastifyPluginAsync } from 'fastify';
 import { join } from 'path';
 import AutoLoad from 'fastify-autoload';
+import fastifyCors from 'fastify-cors';
 
 export const app: FastifyPluginAsync = async (fastify, opts) => {
   fastify.register(AutoLoad, {
     dir: join(__dirname, 'plugins'),
-    options: opts
-  })
+    options: opts,
+  });
 
   fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
-    options: opts
-  })
-}
+    options: opts,
+  });
+
+  // クライアントからアクセスできるようにする
+  // 本番環境ではこの設定良くないかも
+  fastify.register(fastifyCors, {
+    origin: "*",
+  });
+};
