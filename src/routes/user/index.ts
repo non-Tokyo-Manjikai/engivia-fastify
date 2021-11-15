f6ac60dbac98ff3c03d13f82fdf637808ff61b8a
aopontann <aopontan0416@gmail.com>
2021-11-15 22:10:49 +0900

    ✨ Serialization, Validation機能を追加 #16 - ハンドラーを実行する前に実行するイベントを作成し、そのイベントで認証を行う処理を作成した。 - preHandlerイベントでユーザー情報を取得しているので、getUserは削除した。

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,53 +1,105 @@
 import { FastifyPluginAsync } from 'fastify';
 import userPlugin from './service';
+import {
+  UserInfo,
+  userPutBodySchema,
+  getUserResponse,
+  updateUserResponse,
+  deleteUserResponse,
+  UserPutBody,
+} from './schema';
 
-type PutBody = { name: string; image: string; base64Image?: string };
+declare module 'fastify' {
+  interface FastifyRequest {
+    userInfo: UserInfo;
+  }
+}
 
 const user: FastifyPluginAsync = async (fastify): Promise<void> => {
-  // Broadcastプラグインを読み込む！
   await fastify.register(userPlugin);
+  fastify.decorateRequest('userInfo', null);
 
-  // ユーザー情報取得(idで取得)
-  fastify.get<{
-    Params: { id: string };
-  }>(`/:id`, async (req, res) => {
-    const { id } = req.params;
-    const resultUser = await fastify.getUser({ id });
-    res.send(resultUser);
-  });
+  fastify.addHook<{ Body: { token?: string } }>(
+    'preHandler',
+    async (req, reply, done) => {
+      const token = req.body?.token || req.headers.authorization?.split(' ')[1];
+      if (!token) {
+        reply.code(403);
+        done(new Error('Specify token'));
+      }
+      const userInfo = await fastify.prisma.user.findUnique({
+        where: { token },
+        select: { id: true, name: true, isAdmin: true, image: true },
+      });
+      if (userInfo) {
+        req.userInfo = userInfo;
+        done();
+      } else {
+        reply.code(403);
+        done(new Error('Invalid token'));
+      }
+    },
+  );
 
   // ユーザー情報取得(bearer tokenで取得)
-  fastify.get(`/`, async (req, res) => {
-    const token = req?.headers?.authorization?.split(' ')[1] as string;
-    const resultUser = await fastify.getUser({ token });
-    res.send(resultUser);
-  });
+  fastify.get(
+    `/`,
+    {
+      schema: {
+        response: {
+          '200': getUserResponse,
+        },
+      },
+    },
+    async (req, res) => {
+      res.send(req.userInfo);
+    },
+  );
 
-  // ユーザー情報更新(bearer tokenで更新)
+  // ユーザー情報更新
   fastify.put<{
-    Body: PutBody;
-  }>(`/`, async (req, res) => {
-    const { name, image, base64Image } = req.body;
-    const token = req?.headers?.authorization?.split(' ')[1] as string;
-    const imageURL = base64Image
-      ? await fastify.uploadIcon({ token, base64: base64Image })
-      : image;
-    const updateUser = await fastify.updateUser({
-      token,
-      name,
-      image: imageURL,
-    });
-    res.send(updateUser);
-  });
+    Body: UserPutBody;
+  }>(
+    `/`,
+    {
+      schema: {
+        body: userPutBodySchema,
+        response: {
+          '200': updateUserResponse,
+        },
+      },
+    },
+    async (req, res) => {
+      const { name, image, base64Image } = req.body;
+      const imageURL = base64Image
+        ? await fastify
+            .uploadIcon({ id: req.userInfo.id, base64: base64Image })
+            .catch(() => {
+              throw new Error(`Image upload failed`);
+            })
+        : image;
+      const updatedUser = await fastify.updateUser({
+        id: req.userInfo.id,
+        name,
+        image: imageURL,
+      });
+      res.send(updatedUser);
+    },
+  );
 
   // ユーザー情報削除(bearer tokenで削除)
-  fastify.delete<{
-    Params: { id: string };
-  }>(`/`, async (req, res) => {
-    const token = req?.headers?.authorization?.split(' ')[1] as string;
-    const deleteUser = await fastify.deleteUser({ token });
-    res.send(deleteUser);
-  });
+  fastify.delete(
+    `/`,
+    {
+      schema: {
+        response: { '200': deleteUserResponse },
+      },
+    },
+    async (req, res) => {
+      const deleteUser = await fastify.deleteUser({ id: req.userInfo.id });
+      res.send(deleteUser);
+    },
+  );
 };
 
 export default user;
