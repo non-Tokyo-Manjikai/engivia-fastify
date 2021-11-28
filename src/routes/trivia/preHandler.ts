a9908c24b6bfb2025f06d7e4605c2e4182134ff8
aopontann <aopontan0416@gmail.com>
2021-11-29 01:27:08 +0900

    ✨ preHandler #32 パスパラメーターに指定したエンジビアIDが存在するかチェックする処理を作成

---
+++ src/routes/trivia/preHandler.ts
@@ -0,0 +1,53 @@
+import { FastifyPluginAsync } from 'fastify';
+import fp from 'fastify-plugin';
+
+export const triviaPreHandlerPlugin: FastifyPluginAsync = fp(async (fastify) => {
+  fastify.addHook<{ Params: { id: number }, Body: { content: string, broadcastId: number } }>(
+    'preHandler',
+    async (req, res) => {
+      // トリビアを作成するとき、既にトリビアが保存されているかチェックする
+      if (req.method === 'POST') {
+        // 管理者はエンジビアを投稿できない
+        if (req.userInfo.isAdmin) {
+          res.code(400);
+          throw new Error('Admin are not allowed to post engivia.');
+        }
+        const resultTrivia = await fastify.prisma.trivia.findFirst({
+          where: {
+            AND: [
+              { userId: req.userInfo.id },
+              { broadcastId: req.body.broadcastId }
+            ]
+          }
+        });
+        // 既に投稿している場合
+        if (resultTrivia) {
+          res.code(400);
+          throw new Error('The engivia has already been posted.');
+        }
+        // 投稿されていない場合
+        return;
+      }
+
+      // 投稿したエンジビアがあるかチェック
+      if (/PUT|DELETE/.test(req.method)) {
+        const resultTrivia = await fastify.prisma.trivia.findUnique({
+          where: { id: Number(req.params.id) },
+        });
+
+        // 指定したIDが存在する場合
+        if (resultTrivia) {
+          // 管理者・投稿したユーザー本人の場合
+          if (req.userInfo.isAdmin || resultTrivia.userId === req.userInfo.id) return;
+          // 他のユーザーが投稿したエンジビアを更新・削除しようとしたとき
+          res.code(400);
+          throw new Error(`It is not possible to update or delete another user's endivia.`);
+        }
+
+        // IDが存在しない場合
+        res.code(400);
+        throw new Error('Engivia not found.');
+      }
+    },
+  )
+});
