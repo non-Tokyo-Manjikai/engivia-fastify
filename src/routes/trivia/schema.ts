7826bdf055ebef8b3f807e14020b369974b818fa
aopontann <aopontan0416@gmail.com>
2021-11-29 01:30:05 +0900

    ⚰️ token #32

--- src/routes/trivia/schema.ts
+++ src/routes/trivia/schema.ts
@@ -3,13 +3,11 @@ import s from "fluent-json-schema";
 export const bodyPostTriviaSchema = s.object()
   .prop('content', s.string().maxLength(100).required())
   .prop('broadcastId', s.number().minimum(1).required())
-  .prop('token', s.string().required())
 
 export const bodyPutTriviaSchema = s.object()
   .prop('content', s.string().maxLength(100))
   .prop('featured', s.boolean())
   .prop('hee', s.number().minimum(0).maximum(100))
-  .prop('token', s.string().required())
 
 export const paramsDeleteTrivia = s.object()
   .prop('id', s.number().minimum(1).required())
