a4b3f28331177828448cd0bdbde814b6c009b10c
aopontann <aopontan0416@gmail.com>
2021-11-29 18:38:45 +0900

    🏷️ 出力スキーマ作成 #32 Type.Optional追加

--- src/routes/trivia/schema.ts
+++ src/routes/trivia/schema.ts
@@ -6,13 +6,22 @@ export const triviaPostBodySchema = Type.Object({
 });
 
 export const triviaPutBpdySchema = Type.Object({
-  content: Type.String({ maxLength: 100 }),
-  featured: Type.Boolean(),
-  hee: Type.Number({ minimum: 1, maximum: 100 }),
+  content: Type.Optional(Type.String({ maxLength: 100 })),
+  featured: Type.Optional(Type.Boolean()),
+  hee: Type.Optional(Type.Number({ minimum: 1, maximum: 100 })),
 });
 
 export const triviaDeleteParamsSchema = Type.Object({
   id: Type.Number({ minimum: 1 }),
+});
+
+export const triviaPostPutDeleteResponseSchema = Type.Object({
+  id: Type.Number(),
+  content: Type.String(),
+  hee: Type.Union([Type.Number(), Type.Null()]),
+  featured: Type.Boolean(),
+  userId: Type.String(),
+  broadcastId: Type.Number()
 });
 
 export type TriviaPostBody = Static<typeof triviaPostBodySchema>;
